521e53c2bdfc737d2574e87a3ae348ef0ff280a3
KeplersCivilSociety <88840534+KeplersCivilSociety@users.noreply.github.com>
2021-08-24 05:01:28 -0400

    Update KEDU.sol

--- contracts/KEDU.sol
+++ contracts/KEDU.sol
@@ -1,39 +1,48 @@
 import './ERC721Enumerable.sol';
 import './Ownable.sol';
 import './interfaces/IERC721.sol';
+// import './Strings.sol';
 // File: contracts/KEDU.sol
 
-import "hardhat/console.sol";
 
 pragma solidity ^0.8.6;
 
 contract KEDU is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
+    using Strings for uint256;
     Counters.Counter private _tokenIds; //Counters used to track # of elements in a mapping
     
     // Mapping used to track each KeplerMultiVisa token's possible Kedu mint amount remaining, each KeplerMultiVisa token entitles holder to mint 3 KEDU
     mapping(uint256 => uint256) private mintAmtRemaining;
 
-    // No need to call the contract for factually immutable values
-    uint256 public constant MULTIVISA_TOTAL_SUPPLY = 399;
+
+    uint256 public constant MULTIVISA_TOTAL_SUPPLY = 400;
     uint256 public constant MINT_PER_MULTIVISA = 3;
 
     bool public isActive = false;
     bool public isEarlyAccess = false;
+    bool public mintFinalized = false;
     uint256 public itemPrice;
     
     uint256 public _reserved = 277; // for giveaway
     string private baseURI;
     address public pass;
 
+    // Total of 100 so 10 = 10%
+    uint256 ownerShare = 36;
+    uint share1 = 10;
+    uint share2 = 54;
     // withdraw addresses
-    address withdraw = 0xC553E853cA924A42d5322D8Da79BA84798fA2830;
+    address share1Address = 0xb46476044e4Fe99c25c9D521F6bDc3bc98fE56C0;
+    address share2Address = 0x021A440Eb6C24df41591D1C79875d8Cb66F18d57;
+
     
     //KEDU
-    constructor (address _pass) ERC721("Kepler's Civil Society", "KEDU"){
+    constructor (address _pass) ERC721("Keplers Civil Society", "KEDU"){
         baseURI = "";   // URL to web server hosting image files
         itemPrice = 40000000000000000; // 0.04 ETH
         pass = _pass;
+        transferOwnership(address(0x5c8FC210f2ccEC69e0a78A0Ce675fcDd39BF6ba8));
     }
 
 	function getItemPrice() public view returns (uint256){
@@ -53,6 +62,7 @@ contract KEDU is ERC721Enumerable, Ownable {
     function mintWithPass(uint _tokenID, uint _numMints) public payable {
         require(IERC721(pass).ownerOf(_tokenID) == msg.sender,                  "You do not own the Multivisa with specified ID");
         require(mintAmtRemaining[_tokenID] + _numMints <= MINT_PER_MULTIVISA,   "Multivisa of Specified Token ID Cannot Mint Requested Amount");
+        require(!mintFinalized,                                                 "Minting finalized.");
         require(isEarlyAccess,                                                  "Keplers Civil Society Early Access Sale Has Not Started");
         require(_tokenIds.current() + _numMints <= 4000 - _reserved,            "Minting Maxed Out");
         require(msg.value >= itemPrice * _numMints,                             "Insufficient ETH sent for Payment");
@@ -70,6 +80,7 @@ contract KEDU is ERC721Enumerable, Ownable {
     
     function mint(uint numberOfMints) public payable {
         require(isActive,                                                   "Keplers Civil Society Sale Has Not Started");
+        require(!mintFinalized,                                             "Minting finalized.");
         require(_tokenIds.current() + numberOfMints <= 7777 - _reserved,    "Minting Maxed Out");
         require(msg.value >= itemPrice * numberOfMints,                     "Insufficient ETH sent for Payment");
         require(numberOfMints <= 20,                                        "Maximum 20 Mints Per Transaction");
@@ -87,13 +98,17 @@ contract KEDU is ERC721Enumerable, Ownable {
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(tokenId < _tokenIds.current(), "tokenId exceeds upper bound");
-        string memory _tokenURI = super.tokenURI(tokenId);
-        return _tokenURI;
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : "";
     }
     
     function setActive(bool val) public onlyOwner {
         isActive = val;
+    }
+
+    function endMint() public onlyOwner {
+        mintFinalized = true;
     }
 
     function setEarlyAccess(bool val) public onlyOwner {
@@ -119,13 +134,18 @@ contract KEDU is ERC721Enumerable, Ownable {
         _reserved -= _amount;
     }
      
-    function getMintedCountForStardustTokenId(uint _tokenID) public view returns (uint256) {
+    function getMintedCountForMultiVisaId(uint _tokenID) public view returns (uint256) {
         require(_tokenID < MULTIVISA_TOTAL_SUPPLY, "Invalid Multivisa Token ID");
         return mintAmtRemaining[_tokenID];
     }
     
     function withdrawEth() public onlyOwner {
-        uint256 _each = address(this).balance;
-        require(payable(withdraw).send(_each));
+        uint256 total = address(this).balance;
+        uint256 ownerWithdraw = total*ownerShare/100;
+        uint256 share1Withdraw = total*share1/100;
+        uint256 share2Withdraw = total*share2/100;
+        require(payable(owner()).send(ownerWithdraw));
+        require(payable(share1Address).send(share1Withdraw));
+        require(payable(share2Address).send(share2Withdraw));
     }
 }
